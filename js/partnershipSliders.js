9b81b7496c6df9e3ba7ded805cd27dd8d56f0580
Anton <merkulovich166@gmail.com>
2025-05-07 00:24:13 +0300

    add

--- js/partnershipSliders.js
+++ js/partnershipSliders.js
@@ -1,13 +1,14 @@
-const benefitSliderParams = {
+const textSliderParams = {
     slidesPerView: 'auto',
     spaceBetween: 32,
     centeredSlides: true,
     freeMode: {
       enabled: true,
     },
-    mousewheel: {
-      enabled: true,
-    },
+    // mousewheel: {
+    //   enabled: true,
+    // },
 }
 
-new Swiper('.benefits__slider', benefitSliderParams)
+new Swiper('.benefits__slider', textSliderParams)
+new Swiper('.ideas__slider', textSliderParams)
